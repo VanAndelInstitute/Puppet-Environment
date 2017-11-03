0a4c73b732d0dcf0200f609f4dc1aa70923fda14
root <matthew.hoffman@vai.org>
2017-11-03 14:36:21 -0400

    More refactoring of capture conf

--- environments/development/modules/facterpath/lib/facter/test.rb
+++ environments/development/modules/facterpath/lib/facter/test.rb
@@ -3,8 +3,8 @@ require 'digest'
 
 class Configuration
   
-  #join_info = Dir.chdir(File.dirname(__FILE__)){File.read("ad_join_info.json")}
-  @@SERVER = "foreman.vai.org"#(JSON.parse(join_info))["server"]
+  join_info = Dir.chdir(File.dirname(__FILE__)){File.read("ad_join_info.json")}
+  @@SERVER = (JSON.parse(join_info))["server"]
 
   @@WINDOWS_PATH = "C:/ProgramData/PuppetLabs/facter/facts.d/"
   @@LINUX_PATH = "/opt/puppetlabs/facter/facts.d/"
@@ -19,6 +19,7 @@ class Configuration
     @drift_file     = prefix + "_drift.json"  
 
     @configuration  = capture_configuration
+    @current_drift  = ""
   end
 
   def run
@@ -57,25 +58,22 @@ class Configuration
     
     File.delete(@config_file, @drift_file) if(response.nil? || response.empty?)
 
-    saved_configuration = facter_call(:packages).to_s.gsub("\s","")
-    current_configuration = (JSON.parse(@configuration)["packages"]).to_s.gsub("\s","")
+    saved_configuration = facter_call(:packages)
+    current_configuration = JSON.parse(@configuration)["packages"] 
+    
     if(current_configuration == saved_configuration) 
       puppet_call(:notice, "No drift detected on #{@fqdn}.")
     else
-      puppet_call(:notice, "curr: #{current_configuration} ec\n\n")
-      puppet_call(:notice, "saved: #{saved_configuration} es")
-
-      
       current_array = current_configuration.to_a
       saved_array = saved_configuration.to_a
       difference = (current_array - saved_array) + (saved_array - current_array)
-
+      
       difference.each do |d|
         msg = "#{d["name"]} #{d["version"]}"
 
-        if !(current_array.to_s.include? saved_array.to_s)
+        if !(current_array.include? saved_array)
           msg << " not found on #{@fqdn}."
-        elsif !(saved_array.to_s.include? d)
+        elsif !(saved_array.include? d)
           msg << " installed on #{@fqdn} after configuration."
         else
           current_array.each do
@@ -86,15 +84,19 @@ class Configuration
         end
 
         puppet_call(:notice, "#{msg}") unless msg.to_s.empty?
-        @current_drift += msg.to_s unless @current_drift.include? msg.to_s
+        @current_drift += msg.to_s unless(@current_drift.include?(msg.to_s))
       end
 
-      prev_drift = facter_call(:drift)
-      prev_drift = @current_drift if prev_drift.strip.empty?
+      #prev_drift = facter_call(:drift)
+      prev_drift = @current_drift #if prev_drift.strip.empty?
 
-      drift?(prev_drift, @current_drift) ? puppet_call(:warn) : puppet_call(:err)
-
-      msg = "Wow! Drift detected on #{@fqdn}"
+      msg = "Drift detected on #{@fqdn}."
+      if drift?(prev_drift, @current_drift)
+        puppet_call(:warn, msg)
+      else
+        puppet_call(:err, msg)
+      end
+    
     end
   end
   
@@ -117,16 +119,16 @@ class Configuration
     list = filebucket_request(:list)
     restored = false
 
-    unless list.nil? || list.empty?
+    unless(list.nil? || list.empty?)
       cached_files = list.split("\n")
 
       cached_files.reverse_each do |line|
-        next if !(line.include? @config_file) || restored
+        next if(!(line.include? @config_file) || restored)
 
         sum = line.split(" ")[0]
         response = filebucket_request(:get, sum: sum)
 
-        unless response.nil? || response.empty? || restored
+        unless(response.nil? || response.empty? || restored)
 
           restored = true
           filebucket_request(:restore, file: @config_file, sum: sum)
@@ -146,7 +148,7 @@ class Configuration
     when /local_backup/
       `#{cmd} filebucket backup #{file} -l`
     when /remote_backup/
-      `#{cmd} filebucket backup #{file} --server #{server}`
+      `#{cmd} filebucket backup #{file} --server #{@@SERVER}`
     when /list/ 
       `#{cmd} filebucket -l list`
     end
@@ -172,16 +174,16 @@ class Configuration
 
     name = 
       if @os == 'darwin'
-       (pack.to_s.match(/.*:/).to_s).gsub(/:/, "").strip 
+        pack[/.*:/].tr!(':', "") 
       else
-        ((pack.match(/\'.*\':/)).to_s).gsub(/\'|:/, "")
+        pack[/\'.*\'/].tr!("'", "")
       end
 
     version = 
       if @os == 'darwin'
-        (pack.to_s.match(/Version:.*/).to_s).gsub(/Version:/, "").strip 
+        pack[/Version:.*/].to_s.tr!("Version:", "") 
       else
-        ((pack.match(/\'.*\',/)).to_s).gsub(/\'|,/, "")
+        pack[/\'.*\',/].to_s.tr!("',", "")
       end
     
     return {"name" => name, "version" => version}
